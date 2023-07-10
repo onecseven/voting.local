3896d98db6bcef3c4314b96f2022a57c3ed8ed45
tatiana talavera <iced.tati@gmail.com>
2023-07-10 18:35:52 -0400

    major changes forgot to commit

--- src/data.ts
+++ src/data.ts
@@ -2,34 +2,50 @@ import { Poll, PollOption, Ranking } from "./types.js"
 
 class PollHolder {
   polls = new Map<string, Poll>()
-  
+
   add_poll(name: string, options: PollOption[]): boolean {
+    if (this.polls.size > 20) {
+      delete this.polls
+      this.polls = new Map<string, Poll>()
+    }
     if (this.polls.has(name)) return false
-    this.polls.set(name, {name, options, rankings: new Map() })
+    this.polls.set(name, { name, options, rankings: new Map() })
     return true
   }
 
-  add_ranking(poll: string, user: string, ranking: Ranking<PollOption>): boolean {
+  add_ranking(
+    poll: string,
+    user: string,
+    ranking: Ranking<PollOption>
+  ): boolean {
     if (this.polls.has(poll)) {
       let current = this.polls.get(poll)
       current.rankings.set(user, ranking)
       this.polls.set(current.name, current)
-      console.log(this.polls.get(current.name))
       return true
     } else return false
   }
 
   get_poll(name: string): Poll | null {
     if (this.polls.has(name)) {
-      console.log(this.polls.get(name))
       return this.polls.get(name)
     }
     return null
   }
 
+  get_poll_json(name: string) {
+    let gottenpoll = this.get_poll(name)
+    if (gottenpoll)
+      return JSON.stringify({
+        ...gottenpoll,
+        rankings: Object.fromEntries(gottenpoll.rankings),
+      })
+    else return null
+  }
+
   get_all_polls() {
     let result: Poll[] = []
-    this.polls.forEach(entry => result.push(entry))
+    this.polls.forEach((entry) => result.push(entry))
     return result
   }
 
@@ -43,7 +59,9 @@ class PollHolder {
       return false
     }
   }
-
 }
 
-export let singleton = new PollHolder()
+let singleton = new PollHolder()
+
+
+export default singleton
